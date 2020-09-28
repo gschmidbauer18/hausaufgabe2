745e586982c01cf9819f19e75c20105129cfcd93
gschmidbauer18 <Schmidbauer.Gabriel@sus.htl-grieskirchen.at>
2020-09-28 13:22:19 +0200

    initial commit

---
+++ Hausaufgabe2/src/hausaufgabe2/HalloJavamitForEach.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hausaufgabe2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author 43676
+ */
+public class HalloJavamitForEach {
+    
+    public void forEachListe()
+    {
+        List<String> stringListe=new ArrayList<>();
+        stringListe.add("Eins");
+        stringListe.add("Zwei");
+        stringListe.add("Drei");
+        
+        
+    }
+}
